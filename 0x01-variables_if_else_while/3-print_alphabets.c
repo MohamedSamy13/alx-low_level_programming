2b6bb0fdfed8b8d8f2d2d5bffea590733b85cb23
MohamedSamy13 <mohammed7samy777@gmail.com>
2023-08-11 18:17:34 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - print alpha in caps and lower case 
+ *
+ * Return: (0)
+ *
+ */
+int main(void)
+{
+	char Lower = 'a';
+	char Upper = 'A';
+
+	while (Lower <= 'z')
+	{
+		putchar(Lower);
+		Lower++;
+	}
+	while (Upper <= 'z')
+	{
+		putchar(Upper);
+		Upper++;
+	}
+	putchar('\n');
+	return (0);
+}
+
