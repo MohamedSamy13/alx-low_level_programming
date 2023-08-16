53bf21869d3c8052a243410a2311946eda81fb46
MohamedSamy13 <mohammed7samy777@gmail.com>
2023-08-16 16:59:35 -0700

    15. Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	int j;
+	int i;
 	unsigned long int j, k, next, sum;
 
 	j = 1;
 	k = 2;
 	sum = 0;
 
-	for (j = 1; j <= 33; ++j)
+	for (i = 1; i <= 33; ++i)
 	{
 		if (j < 4000000 && (j % 2) == 0)
 		{
